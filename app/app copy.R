662dcf9a61d63a8592964eb1248df194db496402
KatrinaaDing <xiaoding1998@hotmail.com>
2023-09-21 18:07:52 +1000

    add clustered marker

--- app/app copy.R
+++ app/app copy.R
@@ -9,16 +9,17 @@ library(bslib)
 library(maps)
 library(rnaturalearth)
 library(sf)
-library(lwgeom)
+library(htmlwidgets)
+
 
 #########
 # ICONS #
 #########
 
 faceIcons <- iconList(
-  happy = makeIcon("../icons/face-smile-regular.svg", "../icons/face-smile-regular.svg", 24, 24),
-  natural = makeIcon("../icons/face-meh-regular.svg", "../icons/face-meh-regular.svg", 24, 24),
-  sad = makeIcon("../icons/face-frown-regular.svg", "../icons/face-frown-regular.svg", 24, 24)
+  happy = makeIcon("icons/face-smile-regular.svg", "icons/face-smile-regular.svg", 24, 24),
+  natural = makeIcon("icons/face-meh-regular.svg", "icons/face-meh-regular.svg", 24, 24),
+  sad = makeIcon("icons/face-frown-regular.svg", "icons/face-frown-regular.svg", 24, 24)
 )
 
 #########
@@ -37,6 +38,9 @@ faceIcons <- iconList(
 ### Import data
 worldMap <- st_read("../data/countries.geo.json")
 worldHappiness <- read.csv("../data/World Happiness Reports 2013-2023/WorldHappinessIndex2013-2023.csv")
+
+### Filter data
+worldHappiness <- worldHappiness %>% filter(!is.na(worldHappiness$Index))
 
 # worldHappiness <- worldHappiness[complete.cases(worldHappiness$Life.Ladder, worldHappiness$Year), ]
 # worldHappiness$Year <- as.numeric(as.character(worldHappiness$Year))
@@ -146,15 +150,17 @@ server <- function(input, output, session) {
   output$map_happiness <- renderLeaflet({
     dataWithSpatial <- dataWithSpatial %>% filter(Year == input$timeline & !is.na(dataWithSpatial$Index))
     colors <- colorNumeric("Blues", dataWithSpatial$Index)
-    leaflet(dataWithSpatial) %>%
+    leaflet_map <- leaflet(dataWithSpatial) %>%
       addProviderTiles(providers$CartoDB) %>%
       setView(lng = 0, lat = 0, zoom = 2) %>%
       setMaxBounds(lng1 = -180, lat1 = -90, lng2 = 180, lat2 = 90) %>%
       addMarkers(
-        #clusterOptions = markerClusterOptions(),
+        clusterOptions = markerClusterOptions(),
         lng = dataWithSpatial$lng,
         lat = dataWithSpatial$lat,
         icon = ~faceIcons[happiness],
+        options = leaflet::markerOptions(happinessIndex = dataWithSpatial$Index), # Setting happinessIndex
+        
         popup = ~paste("Country:", dataWithSpatial$name, "<br>", "Happiness Score:", dataWithSpatial$Index)
     
       ) %>%
@@ -175,7 +181,45 @@ server <- function(input, output, session) {
 
         title = "Index of Happiness"
       )
+    leaflet_map %>% onRender("
+      function(el, x) {
+        let happyIcon = L.icon({
+            iconUrl: 'icons/face-smile-regular.svg',
+            iconSize: [24, 24]
+        });
+        console.log(happyIcon)
+        let naturalIcon = L.icon({
+            iconUrl: 'icons/face-meh-regular.svg',
+            iconSize: [24, 24]
+        });
 
+        let sadIcon = L.icon({
+            iconUrl: 'icons/face-frown-regular.svg',
+            iconSize: [24, 24]
+        });
+
+        let map = this;
+        map.eachLayer(function(layer) {
+          if (layer instanceof L.MarkerClusterGroup) {
+            layer.options.iconCreateFunction = function(cluster) {
+              let markers = cluster.getAllChildMarkers();
+              let averageHappiness = 0;
+              for (let i = 0; i < markers.length; i++) {
+                averageHappiness += parseFloat(markers[i].options.happinessIndex);
+              }
+              averageHappiness = (averageHappiness / markers.length).toFixed(2);
+              if (averageHappiness > 5) {
+                return happyIcon;
+              } else if (averageHappiness > 3) {
+                return naturalIcon;
+              } else {
+                return sadIcon;
+              }
+            };
+          }
+        });
+      }
+    ")
   })
 }
 
